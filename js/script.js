b69e883d91f854656bd8dd9739ec37a81775e781
xiluna <xilaluna2@gmail.com>
2020-03-05 02:08:45 +0000

    finished + random gif

--- js/script.js
+++ js/script.js
@@ -19,3 +19,16 @@ $(".searchBtn").click(function() {
       $('body').append('<img src ='+ gif + '>');
     });
 });
+
+$(".randomBtn").click(function() {
+  var requestRandomUrl = 'https://api.giphy.com/v1/gifs/random?api_key=rQrXp8Skecms5IYqoh0BS4HeQW9L35LY'
+  fetch(requestRandomUrl)
+    .then(function(response) {
+      return response.json();
+    })
+    .then(function(data) {
+      var gifTwo = data.data.images.original.url
+      console.log(gifTwo);
+      $('body').append('<img src=' + gifTwo + '>');
+    });
+});
